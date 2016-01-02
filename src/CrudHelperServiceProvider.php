f8ff7a49c615773d4220ca9be8034c08c4de1c63
Andreas Beasley <andreas@sapioweb.com>
2016-01-01 21:38:42 -0800

    first commit

---
+++ src/CrudHelperServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sapioweb\CrudHelper;
+
+use Illuminate\Support\ServiceProvider;
+
+class CrudHelperServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
